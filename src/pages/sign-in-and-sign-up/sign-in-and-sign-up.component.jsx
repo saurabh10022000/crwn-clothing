7a954f7971d5ac366ce9510c2c7ee620fc935c31
saurabh10022000 <saurabhjain4916@gmail.com>
2020-05-09 00:04:37 +0530

    complete firebase session

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
 
 
 
@@ -15,6 +17,7 @@ const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
 
     <SignIn />
+    <SignUp />
 
   </div>
 
